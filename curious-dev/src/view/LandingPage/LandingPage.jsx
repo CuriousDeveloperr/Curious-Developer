1b048f228cc2bad3f80841c6d81ace3796540b01
Oscar Pacheco Ortiz <Oscarpa9188@gmail.com>
2022-05-17 17:49:23 -0500

    Updated Styles

--- curious-dev/src/view/LandingPage/LandingPage.jsx
+++ curious-dev/src/view/LandingPage/LandingPage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from '../LandingPage/Header/Header'
+import OpenSource from './SubViews/OpenSource';
+import Community from './SubViews/Community';
 
 export default function LandingPage(){
     return(
         <div>
             <Header />
-            Landing Page Component
+            
+            <OpenSource />
+
+            <Community />
         </div>
     )
 }
---
+++ curious-dev/src/view/LandingPage/SubViews/OpenSource.jsx
@@ -0,0 +1,11 @@
+export default function OpenSource() {
+    return(
+        <div className="my-4 w-10/12 mx-auto border-2  p-3 text-lg">
+            <p>A Open Source Project that enables developers to discover and consruct web3 applications.</p>
+            
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            </p>
+        </div>
+    )
+}
